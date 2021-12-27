6c4f6b1f3981528755a6b1b5b66024a588e2384e
jr7072 <rosales.juan7072@gmail.com>
2021-12-26 18:19:29 -0600

    created file structure and created stylesheets

---
+++ src/Stylesheets/Theme.js
@@ -0,0 +1,24 @@
+const WebTheme = {
+
+    Typography: {
+
+        color: "#FFFFFF",
+        fontFamily: "'Gochi Hand', cursive",
+        fontSize: "16px",
+
+    },
+
+    mainBlocks: {
+
+        backgroundColor: "#21A3DB",
+    }, 
+
+    contrastBlocks: {
+
+        borderRadius: "10px",
+        backgroundColor: "#244DDD"
+    },
+
+};
+
+export default WebTheme;
